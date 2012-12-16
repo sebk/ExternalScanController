e6ea190b0ba092eb02d4e3747195866d2d792026
seb <sek@gobas.de>
2012-12-16 15:29:08 +0100

    comment

--- ExternalScan/Classes/ExternalScanController.h
+++ ExternalScan/Classes/ExternalScanController.h
@@ -8,17 +8,31 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Implement this protocol to receive the data.
+ */
 @protocol ExternalScanControllerDelegate <NSObject>
 
+/**
+ Barcode was scanned successfully.
+ @param content Scanned string
+ */
 -(void)didScanContent:(NSString*)content;
 
 @end
 
 
+/**
+ Controller to handle the support for an external barcode scanner (HID device).
+ */
 @interface ExternalScanController : NSObject <UITextFieldDelegate>
 
 @property(nonatomic, weak) id<ExternalScanControllerDelegate> delegate;
 
+/**
+ Initialize the class and set the visible UIView from the calling class.
+ @param holder UIView from the caller
+ */
 -(id)initWithParentView:(UIView*)holder;
 
 -(void)becomeFirstResponder;
